af522e3c9aeb9e75ef363b198e32e78cd33e6685
kalex3 <31569459+kalex3@users.noreply.github.com>
2025-05-12 08:48:53 +0200

    aliases

--- dot_config/private_fish/conf.d/aliases.fish
+++ dot_config/private_fish/conf.d/aliases.fish
@@ -20,12 +20,17 @@ abbr -a h history
 abbr -a hc history clear
 abbr -a i kitty +kitten icat
 abbr -a ff fastfetch
-abbr -a ta tmux attach -t 
-abbr -a tn tmux new -t 
+abbr -a t tmux
+abbr -a ta tmux attach
+abbr -a tat tmux attach -t 
+abbr -a tn tmux new
+abbr -a tnt tmux new -t 
 abbr -a gpgx gpg -a --export
 abbr -a gpge gpg -a --encrypt -r
 abbr -a gpgd gpg --decrypt
 abbr -a gpgs gpg --clearsign -u
+abbr -a gpgv gpg --verify
+abbr -a gpgi gpg --import
 abbr -a wlc wl-copy
 abbr -a wlp wl-paste
 alias clear="echo 'use CTRL+L'"
